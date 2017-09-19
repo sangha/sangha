15e831cbc0a66ff88ed4d55b9b0052937ae0cbda
Christian Muehlhaeuser <muesli@gmail.com>
2017-09-19 08:36:05 +0200

    Updated database mocks

--- mocks/projects.sql
+++ mocks/projects.sql
@@ -1,5 +1,5 @@
 INSERT INTO projects
-    (id, slug, name, about, website, license, repository, logo)
+    (id, slug, name, about, website, license, repository, logo, created_at)
     VALUES (
         1,
         'toktok',
@@ -8,12 +8,13 @@ INSERT INTO projects
         'https://github.com/muesli/toktok',
         'MIT',
         'https://github.com/muesli/toktok.git',
-        ''
+        '',
+        now()
     );
-INSERT INTO budgets (project_id, parent, name) VALUES (1, 0, 'toktok');
+INSERT INTO budgets (project_id, user_id, parent, name) VALUES (1, null, 0, 'toktok');
 
 INSERT INTO projects
-    (id, slug, name, about, website, license, repository, logo)
+    (id, slug, name, about, website, license, repository, logo, created_at)
     VALUES (
         2,
         'cache2go',
@@ -22,12 +23,13 @@ INSERT INTO projects
         'https://github.com/muesli/cache2go',
         'FOSS',
         'https://github.com/muesli/cache2go.git',
-        ''
+        '',
+        now()
     );
-INSERT INTO budgets (project_id, parent, name) VALUES (2, 0, 'cache2go');
+INSERT INTO budgets (project_id, user_id, parent, name) VALUES (2, null, 0, 'cache2go');
 
 INSERT INTO projects
-    (id, slug, name, about, website, license, repository, logo)
+    (id, slug, name, about, website, license, repository, logo, created_at)
     VALUES (
         3,
         'smolder',
@@ -36,12 +38,13 @@ INSERT INTO projects
         'https://github.com/muesli/smolder',
         'AGPL',
         'https://github.com/muesli/smolder.git',
-        ''
+        '',
+        now()
     );
-INSERT INTO budgets (project_id, parent, name) VALUES (3, 0, 'smolder');
+INSERT INTO budgets (project_id, user_id, parent, name) VALUES (3, null, 0, 'smolder');
 
 INSERT INTO projects
-    (id, slug, name, about, website, license, repository, logo)
+    (id, slug, name, about, website, license, repository, logo, created_at)
     VALUES (
         4,
         'beehive',
@@ -50,6 +53,9 @@ INSERT INTO projects
         'https://github.com/muesli/beehive',
         'AGPL',
         'https://github.com/muesli/beehive.git',
-        '08ca2b0038993af9c2f383f0914004eefd33a6df'
+        '08ca2b0038993af9c2f383f0914004eefd33a6df',
+        now()
     );
-INSERT INTO budgets (project_id, parent, name) VALUES (4, 0, 'beehive');
+INSERT INTO budgets (project_id, user_id, parent, name) VALUES (4, null, 0, 'beehive');
+
+INSERT INTO codes (code, budget_ids, ratios) VALUES ('ABCDEFGH', '{1,2}', '{66,34}');
