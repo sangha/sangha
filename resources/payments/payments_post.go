0c869c5ce25ee3b19383c6ee06fa453e8f401a82
Christian Muehlhaeuser <muesli@gmail.com>
2017-10-17 08:05:02 +0200

    Return parsed payments struct in PaymentResponse

--- resources/payments/payments_post.go
+++ resources/payments/payments_post.go
@@ -2,9 +2,11 @@ package payments
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"gitlab.techcultivation.org/sangha/sangha/db"
 
@@ -43,6 +45,20 @@ func (r *PaymentResource) Post(context smolder.APIContext, data interface{}, req
 	log.Printf("Got payment request: %+v\n", ups)
 
 	ctx := context.(*db.APIContext)
+	payments := struct {
+		Payments []struct {
+			UserID              int64     `json:"user_id"`
+			Amount              float64   `json:"amount"`
+			Currency            string    `json:"currency"`
+			Code                string    `json:"code"`
+			Description         string    `json:"description"`
+			Source              string    `json:"source"`
+			SourceID            string    `json:"source_id"`
+			SourcePayerID       string    `json:"source_payer_id"`
+			SourceTransactionID string    `json:"source_transaction_id"`
+			CreatedAt           time.Time `json:"created_at"`
+		} `json:"payments"`
+	}{}
 	payment := db.Payment{}
 
 	switch ups.Payment.Source {
@@ -57,6 +73,8 @@ func (r *PaymentResource) Post(context smolder.APIContext, data interface{}, req
 		}
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(body))
+
 		if resp.StatusCode != http.StatusOK {
 			smolder.ErrorResponseHandler(request, response, smolder.NewErrorResponse(
 				http.StatusBadRequest,
@@ -66,7 +84,7 @@ func (r *PaymentResource) Post(context smolder.APIContext, data interface{}, req
 			return
 		}
 
-		err = json.Unmarshal(body, &payment)
+		err = json.Unmarshal(body, &payments)
 		if err != nil {
 			smolder.ErrorResponseHandler(request, response, smolder.NewErrorResponse(
 				http.StatusInternalServerError,
@@ -75,6 +93,17 @@ func (r *PaymentResource) Post(context smolder.APIContext, data interface{}, req
 				"PaymentResource POST"))
 			return
 		}
+		fmt.Printf("Payment: %+v\n", payments.Payments[0])
+		payment.UserID = payments.Payments[0].UserID
+		payment.Amount = payments.Payments[0].Amount
+		payment.Currency = payments.Payments[0].Currency
+		payment.Code = payments.Payments[0].Code
+		payment.Description = payments.Payments[0].Description
+		payment.Source = payments.Payments[0].Source
+		payment.SourceID = payments.Payments[0].SourceID
+		payment.SourcePayerID = payments.Payments[0].SourcePayerID
+		payment.SourceTransactionID = payments.Payments[0].SourceTransactionID
+		payment.CreatedAt = payments.Payments[0].CreatedAt
 	default:
 		smolder.ErrorResponseHandler(request, response, smolder.NewErrorResponse(
 			http.StatusBadRequest,
