2e9458d6c2c0bff6fd7198031ffb304c03ba8001
Christian Muehlhaeuser <muesli@gmail.com>
2018-09-20 10:53:02 +0200

    Added search param to codes GET

--- resources/codes/codes_get.go
+++ resources/codes/codes_get.go
@@ -29,6 +29,7 @@ func (r *CodeResource) GetParams() []*restful.Parameter {
 	params = append(params, restful.QueryParameter("user_id", "ID of a user").DataType("string"))
 	params = append(params, restful.QueryParameter("budget_ids[]", "an array of budget IDs").DataType("string"))
 	params = append(params, restful.QueryParameter("ratios[]", "an array of ratios").DataType("int"))
+	params = append(params, restful.QueryParameter("search", "search for name or code of a budget").DataType("string"))
 
 	return params
 }
@@ -53,21 +54,25 @@ func (r *CodeResource) GetByIDs(context smolder.APIContext, request *restful.Req
 
 // Get sends out items matching the query parameters
 func (r *CodeResource) Get(context smolder.APIContext, request *restful.Request, response *restful.Response, params map[string][]string) {
+	ctx := context.(*db.APIContext)
 	resp := CodeResponse{}
 	resp.Init(context)
 
 	userID := params["user_id"]
 	budgetIDs := params["budget_ids[]"]
 	ratios := params["ratios[]"]
-	if len(budgetIDs) > 0 && len(ratios) > 0 {
-		fmt.Println(budgetIDs)
-		fmt.Println(ratios)
 
+	if len(params["search"]) > 0 {
+		codes, _ := ctx.SearchCodes(params["search"][0])
+		for _, code := range codes {
+			resp.AddCode(&code)
+		}
+	} else if len(budgetIDs) > 0 && len(ratios) > 0 {
 		var uid string
 		if len(userID) > 0 {
 			uid = userID[0]
 		}
-		code, err := context.(*db.APIContext).LoadCodeByBudgetsAndRatios(budgetIDs, ratios, uid)
+		code, err := ctx.LoadCodeByBudgetsAndRatios(budgetIDs, ratios, uid)
 		if err != nil {
 			r.NotFound(request, response)
 			return
